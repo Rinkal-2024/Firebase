26f500cdff244980185bea4bc6a34984408d3399
Rinkal Dhankariya <rinkal.dhankariya@iflair.com>
2024-12-26 15:48:56 +0530

    minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ConfigService } from './shared/config.service';
+import * as firebase from 'firebase/compat';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private configService: ConfigService){}
   title = 'fire-base-crud';
+  firebaseConfig : any;
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.firebaseConfig = {
+      apiKey: this.configService.getEnv('firebaseApiKey'),
+      authDomain: this.configService.getEnv('firebaseAuthDomain'),
+      databaseURL: this.configService.getEnv('firebaseDatabaseURL'),
+      projectId: this.configService.getEnv('firebaseProjectId'),
+      storageBucket: this.configService.getEnv('firebaseStorageBucket'),
+      messagingSenderId: this.configService.getEnv('firebaseMessagingSenderId'),
+      appId: this.configService.getEnv('firebaseAppId'),
+      measurementId: this.configService.getEnv('firebaseMeasurementId'),
+    };
+    // firebase.initializeApp(this.firebaseConfig);
+  }
 }
